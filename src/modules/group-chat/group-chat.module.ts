05d3cd3778e68e1cdc18b69ee535e4f58385336a
Luc Huynh Tan <tanluc.jdtech04@gmail.com>
2026-07-10 11:40:59 +0700

    fix: initialize group-chat module with gateway, service, and controller dependencies

--- src/modules/group-chat/group-chat.module.ts
+++ src/modules/group-chat/group-chat.module.ts
@@ -1,12 +1,13 @@
 import { PrismaModule } from '@/shared/database/prisma.module';
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { GroupsModule } from '../groups/groups.module';
 import { GroupChatController } from './group-chat.controller';
 import { GroupChatGateway } from './group-chat.gateway';
 import { GroupChatService } from './group-chat.service';
 
 @Module({
-  imports: [PrismaModule, JwtModule.register({})],
+  imports: [PrismaModule, JwtModule.register({}), GroupsModule],
   controllers: [GroupChatController],
   providers: [GroupChatService, GroupChatGateway],
   exports: [GroupChatService],
