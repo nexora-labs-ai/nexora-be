7ff4750bcd275cc2b664032d8b28b76f3c7bffeb
An <quyan2004@gmail.com>
2026-07-17 10:22:28 +0700

    fix: relocate isemailorusernaem validator

--- src/modules/groups/presentation/invite-member.dto.ts
+++ src/modules/groups/presentation/invite-member.dto.ts
@@ -1,45 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsNotEmpty,
-  MaxLength,
-  ValidationArguments,
-  ValidationOptions,
-  ValidatorConstraint,
-  ValidatorConstraintInterface,
-  isEmail,
-  registerDecorator,
-} from 'class-validator';
-import {
-  USERNAME_INVALID_MESSAGE,
-  USERNAME_REGEX,
-} from '../../../shared/common/validators/validation.constants';
-
-@ValidatorConstraint({ name: 'isEmailOrUsername', async: false })
-export class IsEmailOrUsernameConstraint implements ValidatorConstraintInterface {
-  validate(text: string, args: ValidationArguments) {
-    if (typeof text !== 'string') return false;
-    if (text.includes('@')) {
-      return isEmail(text);
-    }
-    return USERNAME_REGEX.test(text);
-  }
-
-  defaultMessage(args: ValidationArguments) {
-    return USERNAME_INVALID_MESSAGE;
-  }
-}
-
-export function IsEmailOrUsername(validationOptions?: ValidationOptions) {
-  return (object: object, propertyName: string) => {
-    registerDecorator({
-      target: object.constructor,
-      propertyName: propertyName,
-      options: validationOptions,
-      constraints: [],
-      validator: IsEmailOrUsernameConstraint,
-    });
-  };
-}
+import { IsNotEmpty, MaxLength } from 'class-validator';
+import { IsEmailOrUsername } from '../../../shared/common/validators/is-email-or-username.validator';
 
 export class InviteMemberDto {
   @ApiProperty({ description: 'Email or Username of the user to invite' })
